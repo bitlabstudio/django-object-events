0190a0f5b28802777c9c999c9d8244176915acf4
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2013-03-05 18:51:36 +0100

    removed datamigration because it threw errors

--- object_events/migrations/0004_auto__del_field_objectevent_type__add_field_objectevent_event_type.py
+++ object_events/migrations/0004_auto__del_field_objectevent_type__add_field_objectevent_event_type.py
@@ -2,18 +2,29 @@
 # -*- coding: utf-8 -*-
 import datetime
 from south.db import db
-from south.v2 import DataMigration
+from south.v2 import SchemaMigration
 from django.db import models
 
-class Migration(DataMigration):
+
+class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        "Write your forwards methods here."
-        db.rename_column('object_events_objectevent', 'type', 'event_type')
+        # Deleting field 'ObjectEvent.type'
+        db.delete_column('object_events_objectevent', 'type_id')
+
+        # Adding field 'ObjectEvent.event_type'
+        db.add_column('object_events_objectevent', 'event_type',
+                      self.gf('django.db.models.fields.related.ForeignKey')(default='', related_name='events', to=orm['object_events.ObjectEventType']),
+                      keep_default=False)
+
 
     def backwards(self, orm):
-        "Write your backwards methods here."
-        db.rename_column('object_events_objectevent', 'event_type', 'type')
+
+        # User chose to not deal with backwards NULL issues for 'ObjectEvent.type'
+        raise RuntimeError("Cannot reverse this migration. 'ObjectEvent.type' and its values cannot be restored.")
+        # Deleting field 'ObjectEvent.event_type'
+        db.delete_column('object_events_objectevent', 'event_type_id')
+
 
     models = {
         'auth.group': {
@@ -63,7 +74,7 @@ class Migration(DataMigration):
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
             'read_by_user': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'events'", 'null': 'True', 'to': "orm['auth.User']"})
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'object_events'", 'null': 'True', 'to': "orm['auth.User']"})
         },
         'object_events.objecteventtype': {
             'Meta': {'object_name': 'ObjectEventType'},
@@ -73,4 +84,3 @@ class Migration(DataMigration):
     }
 
     complete_apps = ['object_events']
-    symmetrical = True
